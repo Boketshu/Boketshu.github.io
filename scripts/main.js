47938c2650e9ebf45108476eb616bc3929592f85
Hermes nkenda <158804078+Boketshu@users.noreply.github.com>
2024-04-14 01:07:07 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// Drop menu lors de la responsive
+const btn = document.getElementById('menu')
+const nav = document.getElementById('nav')
+
+btn.addEventListener('click', () => {
+    nav.classList.toggle('mobile')
+});
+// Drop menu lors de la responsive
+
+
